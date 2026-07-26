d709376927a02c608591f419005f730b38b84e9f
Deezinn <developer.andrelz@gmail.com>
2026-07-26 19:20:23 -0400

    feat: create module to export functions

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,3 +1,4 @@
-from .request import run_requests
+from .request import extrat_by_request
+from .aiohttp import extract_by_aiohttp
 
-__all__ = ['run_requests']
+__all__ = ['extrat_by_request', 'extract_by_aiohttp']
