0402ff305be58eae23b334e7ae6cd4264847794f
Deezinn <developer.andrelz@gmail.com>
2026-07-28 08:38:32 -0400

    feat: apply new module

--- examples/__init__.py
+++ examples/__init__.py
@@ -1,4 +1,5 @@
-from .request import extrat_by_request
 from .aiohttp import extract_by_aiohttp
+from .niquests import extract_by_niquests
+from .request import extrat_by_request
 
-__all__ = ['extrat_by_request', 'extract_by_aiohttp']
+__all__ = ['extract_by_aiohttp', 'extract_by_niquests', 'extrat_by_request']
